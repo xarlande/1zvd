e5ca2b989407c1e528d92305ab1f266aa41036bc
Bogdan <0966312126a@gmail.com>
2022-11-25 13:07:57 +0200

    #

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const classHeader = document.querySelector('.button-header')
 const buttonHeader = document.querySelector(".header__burger-btn")
+const containerHeader = document.querySelector('.container.header-container')
 
 
 buttonHeader.addEventListener('click', () => {
     classHeader.classList.toggle('active')
+    containerHeader.classList.toggle('active')
 })
